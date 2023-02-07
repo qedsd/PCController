cf03be41bbb5692acdc99e492852d4a0aeec8b61
QEDSD <2998739882@qq.com>
2023-02-07 17:56:11 +0800

    UserUI暂存

---
+++ PCController.Core/Models/KeyboardItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCController.Core.Models
+{
+    /// <summary>
+    /// 键盘按键
+    /// </summary>
+    public class KeyboardItem
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Note { get; set; }
+    }
+}
--- PCController.UserUI/MainPage.xaml.cs
+++ PCController.UserUI/MainPage.xaml.cs
@@ -9,16 +9,48 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void ImageButton_Pressed(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            (sender as ImageButton).WidthRequest = 120;
+            (sender as ImageButton).HeightRequest = 120;
         }
+
+        private void ImageButton_Released(object sender, EventArgs e)
+        {
+            (sender as ImageButton).WidthRequest = 100;
+            (sender as ImageButton).HeightRequest = 100;
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            //DisplayAlert("1", "1","1");
+        }
+
+        private void TapGestureRecognizer_Tapped2(object sender, EventArgs e)
+        {
+            //DisplayAlert("2", "2", "2");
+        }
+
+        private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+        {
+            Console.WriteLine("111");
+        }
+
+        private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            Console.WriteLine(e.TotalX);
+        }
+
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
+
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
     }
 }
